c8724bbb0851f7e4657e4e97dde2680c74d820d3
Alejandro Garay <alejandrogaray1966@gmail.com>
2025-07-06 03:11:13 -0300

    Lista de productos

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProdList } from "./prod-list/prod-list";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ProdList],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected title = 'elfuerte';
+  protected title = 'Distribuidora: El Fuerte';
 }
