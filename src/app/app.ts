994440da55d8c08b2d019f0501e20ffead0fdda5
Alejandro Garay <alejandrogaray1966@gmail.com>
2025-07-20 00:48:27 -0300

    se crea el icono del carrito

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
 import { DatePipe } from '@angular/common';
+import { ProdCarri } from './prod-carri';
+import { Product } from './prod-list/Product';
 
 @Component({
   selector: 'app-root',
@@ -10,6 +12,19 @@ import { DatePipe } from '@angular/common';
 })
 
 export class App {
+
   protected title = 'Distribuidora: El Fuerte';
-  today: Date = new Date();
+  
+  protected today: Date = new Date();
+
+  protected totalItems: number = 0;
+
+  constructor(private prodCarri: ProdCarri) {}
+
+  ngOnInit(): void {
+    this.prodCarri.listProduct.subscribe((products: Product[]) => {
+      this.totalItems = products.reduce((sum, p) => sum + p.quantity, 0);
+    });
+  }
+  
 }
